fddf8cb77e9ef7f12924bdd88f1b5d7d9ffc4c9b
maharaja.bat <maharaja.bat@gmail.com>
2024-05-04 11:38:58 +0530

    loading icon

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,22 +2,25 @@ import React, { useState } from 'react'
 import './Login.css'
 import {useNavigate} from 'react-router-dom'
 import {useCookies} from 'react-cookie'
+import { RiMovie2Fill } from "react-icons/ri";
 export default function Login() {
     const [cookie,setCookie]=useCookies([])
     const[login,setlogin]=useState(true);
     const[username,setname]=useState("");
     const[email,setemail]=useState("");
     const[error,seterror]=useState(false);
-    const [success,setsuccess]=useState(false)
+    const [success,setsuccess]=useState(false);
+    const [loading,setloading]=useState(false);
     const[reg,setreg]=useState(false);
     const[msg,setmsg]=useState(false);
     const nav=useNavigate();
     const[pw,setpw]=useState("");
     const handlesubmit=async (e)=>{
         e.preventDefault();
+        setloading(true);
         const log=document.querySelector("#login");
         log.disabled=true;
-        log.style.opacity="70%";
+        log.style.opacity="50%";
        try{
         const login=await fetch(`${import.meta.env.VITE_BACKEND}/validate`,{
             method:"POST",
@@ -41,6 +44,7 @@ export default function Login() {
             setsuccess(false)
             seterror(true);
         }
+        setloading(false);
        }
        catch(error){
         console.log("Error at Api");
@@ -48,9 +52,10 @@ export default function Login() {
     }
     const handleregister=async (e)=>{
         e.preventDefault();
+        setloading(true);
         const reg=document.querySelector("#register");
         reg.disabled=true;
-        reg.style.opacity="70%";
+        reg.style.opacity="50%";
         try{
        
          const login=await fetch('https://moviecult-backend.onrender.com/adduser',{
@@ -73,6 +78,7 @@ export default function Login() {
              setreg(true);
              setmsg(false);
          }
+         setloading(false)
         }
         catch(error){
          console("Error at Api");
@@ -83,10 +89,10 @@ export default function Login() {
         <div className="enterform">
             {login && <form id='loginform'  onSubmit={handlesubmit}>
                 <label >Email</label>
-            <input type="email" className="email" onChange={(e)=>{setemail(e.target.value); seterror(false);log.disabled=false;
+            <input type="email" className="email" onChange={(e)=>{setemail(e.target.value); seterror(false); const log=document.querySelector("#login");log.disabled=false;
         log.style.opacity="100%";}} style={{"textTransform":"lowercase"}} required/>
             <label >Password</label>
-            <input type="password" className="pw" onChange={(e)=>{setpw(e.target.value); seterror(false);log.disabled=false;
+            <input type="password" className="pw" onChange={(e)=>{setpw(e.target.value); seterror(false);  const log=document.querySelector("#login");log.disabled=false;
         log.style.opacity="100%";}} style={{"textTransform":"none"}} required/>
             <button type="submit" className="login" id='login'>Login</button>
             </form>
@@ -104,6 +110,9 @@ export default function Login() {
             <button type="submit" className="login" id='register'>Register</button>
             </form>
 }
+      {loading &&
+        <div id='load'><RiMovie2Fill /></div>
+      }
        {error && 
        <span className="error" style={{background:"black",color:"red",borderRadius:"10px",padding:"10px"}}>Invalid UserName or Password</span>
        }
--- src/components/User.jsx
+++ src/components/User.jsx
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from 'react'
 import { useCookies } from 'react-cookie'
 import './User.css'
 import Nav from './Nav'
+import { RiMovie2Fill } from "react-icons/ri";
 export default function User() {
   const [cookie,setCookie,removeCookie]=useCookies([]);
   const [success,setsuccess]=useState(false);
+  const[loading,setloading]=useState(false)
   const [fail,setfail]=useState(false);
   const [username,setname]=useState("");
   const [email,setemail]=useState("");
@@ -23,9 +25,10 @@ export default function User() {
 
   const handlesubmit=(e)=>{
     e.preventDefault()
+    setloading(true);
     const upd=document.querySelector("#update");
     upd.disabled=true;
-    upd.style.opacity="70%";
+    upd.style.opacity="50%";
          const username=document.querySelector("#username").value;
          const email=document.querySelector("#email").value;
          const password=document.querySelector("#password").value;
@@ -50,7 +53,10 @@ export default function User() {
             setfail(true)
             setsuccess(false)
           }
-         });
+          setloading(false);
+         }
+        );
+         
        
   }
   const handlelogout=()=>{
@@ -66,15 +72,18 @@ export default function User() {
       <span id='userhead'>User Profile</span>
       <form action="" className="userform" onSubmit={handlesubmit}>
         <label htmlFor="username">UserName</label>
-        <input type="text" value={username} id="username"  style={{textTransform:"none"}} onChange={(e)=>{setname(e.target.value);setfail(false);setsuccess(false);upd.disabled=false;
+        <input type="text" value={username} id="username"  style={{textTransform:"none"}} onChange={(e)=>{setname(e.target.value);setfail(false);setsuccess(false); const upd=document.querySelector("#update");upd.disabled=false;
     upd.style.opacity="100%";}} required/>
         <label htmlFor="email">Email</label>
-        <input type="email" value={email} id="email" style={{textTransform:"lowercase"}} onChange={(e)=>{setemail(e.target.value);setfail(false);setsuccess(false);upd.disabled=false;
+        <input type="email" value={email} id="email" style={{textTransform:"lowercase"}} onChange={(e)=>{setemail(e.target.value);setfail(false);setsuccess(false); const upd=document.querySelector("#update");upd.disabled=false;
     upd.style.opacity="100%";}} required />
         <label htmlFor="password">Password</label>
-        <input type="password" value={password}  id="password"  style={{textTransform:"none"}} onChange={(e)=>{setpassword(e.target.value);setfail(false);setsuccess(false);upd.disabled=false;
+        <input type="password" value={password}  id="password"  style={{textTransform:"none"}} onChange={(e)=>{setpassword(e.target.value);setfail(false);setsuccess(false); const upd=document.querySelector("#update");upd.disabled=false;
     upd.style.opacity="100%";}} required/>
         <button type='submit' id="update">Update</button>
+        {loading &&
+        <div id='load'><RiMovie2Fill /></div>
+        }
         {success && 
         <span id="success">User updated successfully</span>
         }
